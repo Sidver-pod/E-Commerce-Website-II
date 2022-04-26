3a02e73494f45006169b3612bb628437a3c9dc9a
Sidver-pod <75096302+Sidver-pod@users.noreply.github.com>
2022-04-26 11:35:26 +0530

    Added Pagination
    + offset & limit (i.e. skip & limit)

--- controllers/eCommerceWebsite.js
+++ controllers/eCommerceWebsite.js
@@ -1,11 +1,24 @@
 const Product = require('../models/product');
 
-const Cart = require('../models/cart');
+const ITEMS_PER_PAGE = 9;
+const ITEMS_IN_CART = 3;
 
 exports.getStore = (req, res, next) => {
-    Product.findAll()
+
+    const currentPageNumber = req.body.currentPageNumber;
+    let totalItems;
+
+    Product.count()
+     .then(numOfProducts => {
+         totalItems = numOfProducts;
+
+         return Product.findAll({
+            offset: (currentPageNumber - 1) * ITEMS_PER_PAGE,
+            limit: ITEMS_PER_PAGE
+        })  
+     })
      .then(products => {
-         if(products.length === 0) {
+         if(totalItems.length === 0) {
              res.json({
                  'check': 'false'
              });
@@ -13,7 +26,9 @@ exports.getStore = (req, res, next) => {
          else {
              res.json({
                  'check': 'true',
-                 'products': products
+                 'totalItems': totalItems,
+                 'products': products,
+                 'ITEMS_PER_PAGE': ITEMS_PER_PAGE
              });
          }
      })
@@ -23,12 +38,37 @@ exports.getStore = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
+    const currentCartPageNumber = req.body.currentCartPageNumber;
+    let fetchedCart, totalItems, totalAmount = 0;
+
     req.user.getCart()
      .then(cart => {
+         fetchedCart = cart;
+
          return cart.getProducts();
      })
      .then(products => {
-         res.json(products);
+         totalItems = products.length;
+
+         // finding total amount sum (for Front-End!)
+         for(let i=0; i<totalItems; i++) {
+             let price = products[i].dataValues.price;
+             let qty = products[i].dataValues.cartItem.dataValues.quantity;
+             totalAmount += price * qty;
+         }
+         
+         return fetchedCart.getProducts({
+             offset: (currentCartPageNumber - 1) * ITEMS_IN_CART,
+             limit: ITEMS_IN_CART
+         });
+     })
+     .then(products => {
+         res.json({
+             'totalItems': totalItems,
+             'ITEMS_IN_CART': ITEMS_IN_CART,
+             'totalAmount': totalAmount,
+             'products': products
+         });
      })
      .catch(err => console.log(err));
 };
@@ -36,6 +76,7 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
     const prodId = req.body.prodId;
     const qty = req.body.quantity;
+    let totalItems, totalAmount = 0;
 
     // error check!
     if(!prodId) {
@@ -49,7 +90,8 @@ exports.postCart = (req, res, next) => {
     req.user.getCart()
      .then(cart => {
          fetchedCart = cart;
-         return cart.getProducts({ where: { id: prodId } });
+
+         return fetchedCart.getProducts({ where: { id: prodId } });
      })
      .then(products => {
          let product;
@@ -59,38 +101,51 @@ exports.postCart = (req, res, next) => {
          }
 
          // if product already exists in the Cart
-         if(product) {
-             return product;
-         }
+         if(product) return product;
          // if the product is being added to the cart for the first time!
-         else {
-            return Product.findByPk(prodId)
-         }
+         else return Product.findByPk(prodId);
      })
      .then(product => {
-        fetchedCart.addProduct(product, { through: { quantity: qty } });
-        res.json({
-            'check': 'true'
-        });
-    })
-    .catch(err => console.log(err));
+        return fetchedCart.addProduct(product, { through: { quantity: qty } });
+     })
+     .then(result => {
+        return fetchedCart.getProducts();
+     })
+     .then(products => {
+         totalItems = products.length; // will help in determining cart page number buttons
+
+         // finding total amount sum (for Front-End!)
+         for(let i=0; i<totalItems; i++) {
+             let price = products[i].dataValues.price;
+             let qty = products[i].dataValues.cartItem.dataValues.quantity;
+             totalAmount += price * qty; // summing to find the pay amount
+         }
+
+         res.json({
+             'check': 'true',
+             'totalItems': totalItems,
+             'ITEMS_IN_CART': ITEMS_IN_CART,
+             'totalAmount': totalAmount
+         });
+     })
+     .catch(err => console.log(err));
 };
 
 exports.removeCartItem = (req, res, next) => {
     const prodId = req.body.prodId;
+    let fetchedCart;
 
     req.user.getCart()
      .then(cart => {
+         fetchedCart = cart;
          return cart.getProducts({ where: { id: prodId } });
      })
      .then(products => {
          // product exists in the database!
          if(products.length > 0) {
             let product = products[0];
-            product.cartItem.destroy();
-            res.json({
-                'check': 'true'
-            });
+
+            return product.cartItem.destroy();
          }
          // product does not exist in the database!!
          else {
@@ -99,5 +154,50 @@ exports.removeCartItem = (req, res, next) => {
              });
          }
      })
+     .then(result => {
+        return fetchedCart.getProducts();
+     })
+     .then(products => {
+         let totalItems = products.length; // will help in determining cart page number buttons
+         let totalAmount = 0;
+
+         // finding total amount sum (for Front-End!)
+         for(let i=0; i<totalItems; i++) {
+             let price = products[i].dataValues.price;
+             let qty = products[i].dataValues.cartItem.dataValues.quantity;
+             totalAmount += price * qty; // summing to find the pay amount
+         }
+
+         res.json({
+            'check': 'true',
+            'totalItems': totalItems,
+            'totalAmount': totalAmount,
+            'ITEMS_IN_CART': ITEMS_IN_CART
+         });
+     })
      .catch(err => console.log(err));
+};
+
+// specifically for checking if an item is in the Cart
+exports.checkCartItem = (req, res, next) => {
+    const prodId = req.body.prodId;
+
+    req.user.getCart()
+     .then(cart => {
+         return cart.getProducts({ where: { id: prodId } });
+     })
+     .then(products => {
+         // item already exists in the Cart!
+         if(products.length > 0) {
+             res.json({
+                 'check': 'true'
+             });
+         }
+         else {
+             res.json({
+                 'check': 'false'
+             });
+         }
+     })
+     .catch(err => console.log(err))
 };
