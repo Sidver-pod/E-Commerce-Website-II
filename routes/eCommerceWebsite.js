4fd242b6733ce161e1f47c9653254be75c19fadc
Sidver-pod <75096302+Sidver-pod@users.noreply.github.com>
2022-04-26 11:41:33 +0530

    Updated all to 'POST request' for Pagination
    + Done so as to be able to carry "current page number" for Pagination

--- routes/eCommerceWebsite.js
+++ routes/eCommerceWebsite.js
@@ -4,9 +4,10 @@ const router = express.Router();
 
 const eCommerceWebsiteController = require('../controllers/eCommerceWebsite');
 
-router.get('/store', eCommerceWebsiteController.getStore);
+router.post('/store', eCommerceWebsiteController.getStore);
 router.post('/cart/remove', eCommerceWebsiteController.removeCartItem);
-router.get('/cart', eCommerceWebsiteController.getCart);
+router.post('/get-cart', eCommerceWebsiteController.getCart);
 router.post('/cart', eCommerceWebsiteController.postCart);
+router.post('/check-cart', eCommerceWebsiteController.checkCartItem);
 
 module.exports = router;
