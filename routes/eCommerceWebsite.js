275de06186c165907b4e6ad3312b7e7c6afd277d
Sidver-pod <75096302+Sidver-pod@users.noreply.github.com>
2022-05-03 19:59:53 +0530

    Updated with Order table

--- routes/eCommerceWebsite.js
+++ routes/eCommerceWebsite.js
@@ -9,5 +9,7 @@ router.post('/cart/remove', eCommerceWebsiteController.removeCartItem);
 router.post('/get-cart', eCommerceWebsiteController.getCart);
 router.post('/cart', eCommerceWebsiteController.postCart);
 router.post('/check-cart', eCommerceWebsiteController.checkCartItem);
+router.post('/place-order', eCommerceWebsiteController.postOrder);
+router.get('/get-order', eCommerceWebsiteController.getOrder);
 
 module.exports = router;
